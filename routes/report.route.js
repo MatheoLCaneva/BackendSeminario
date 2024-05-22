617111f9a9d9f7f3f131911f61c7698bd404a389
Matheo Leonel Caneva Labonia <MC_E97162@redlink.com.ar>
2024-05-22 08:52:19 -0300

    Mail Controller

--- routes/report.route.js
+++ routes/report.route.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var ReportController = require('../controllers/report.controller');
-var MailController = require('../controllers/mail.controller');
+// var MailController = require('../controllers/mail.controller');
 var Authorization = require('../auth/authorization'); 
 
 // Authorize each API with middleware and map to the Controller Functions
